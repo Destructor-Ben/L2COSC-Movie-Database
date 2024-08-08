e273eb24440fec980bb7a40d27fb177d4c802611
Destructor_Ben <ben.honeyfield@gmail.com>
2024-08-09 10:10:51 +1200

    Get basic buffer working for console

--- mdb.py
+++ mdb.py
@@ -1,4 +1,17 @@
 import mdb_console as console
+import time
 
 console.clear()
 print(console.get_size())
+
+time.sleep(1)
+
+console.clear()
+console.recreate_buffer()
+# Rendering goes here
+console.buffer[0][0] = "a"
+console.display()
+
+time.sleep(2)
+
+console.clear()
--- mdb_console.py
+++ mdb_console.py
@@ -1,8 +1,30 @@
 import os
 
+buffer = []
+width = 0
+height = 0
+
 def clear():
     print(chr(27) + "c", "")
 
 def get_size():
     size = os.get_terminal_size()
     return (size.columns, size.lines)
+
+def recreate_buffer():
+    global buffer
+    global width
+    global height
+
+    size = get_size()
+    width = size[0]
+    height = size[1]
+    buffer = [[" " for y in range(height)] for x in range(width)]
+
+def display():
+    width = get_size()[0]
+    height = get_size()[1]
+
+    for y in range(height):
+        for x in range(width):
+            print(buffer[x][y], end="", flush=False)
