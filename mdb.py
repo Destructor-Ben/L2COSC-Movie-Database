43cd1edd7f4982bf07b1183ec276abee3d8f0af8
Destructor_Ben <ben.honeyfield@gmail.com>
2024-08-09 10:25:23 +1200

    Rework the console code so we can have input

--- mdb.py
+++ mdb.py
@@ -1,25 +1,18 @@
 import mdb_console as console
-import time
 
-console.clear()
-console.recreate_buffer()
+def render():
+    for x in range(console.width):
+        console.buffer[x][0] = "-"
+        console.buffer[x][-1] = "-"
 
-# Rendering goes here
-for x in range(console.width):
-    console.buffer[x][0] = "-"
-    console.buffer[x][-1] = "-"
+    for y in range(console.height):
+        console.buffer[0][y] = "|"
+        console.buffer[-1][y] = "|"
 
-for y in range(console.height):
-    console.buffer[0][y] = "|"
-    console.buffer[-1][y] = "|"
+    console.buffer[0][0] = "+"
+    console.buffer[-1][0] = "+"
+    console.buffer[0][-1] = "+"
+    console.buffer[-1][-1] = "+"
 
-console.buffer[0][0] = "+"
-console.buffer[-1][0] = "+"
-console.buffer[0][-1] = "+"
-console.buffer[-1][-1] = "+"
-
+console.setup(render)
 console.display()
-
-input()
-
-console.clear()
--- mdb_console.py
+++ mdb_console.py
@@ -3,13 +3,19 @@ import os
 buffer = []
 width = 0
 height = 0
+user_input = None
+render = None # Callback to render
+
+def setup(render_callback):
+    global render
+    render = render_callback
 
 def clear():
     print(chr(27) + "c", end="")
 
 def get_size():
     size = os.get_terminal_size()
-    return (size.columns, size.lines)
+    return (size.columns, size.lines - 1) # Remove one so we can have a line for input
 
 def recreate_buffer():
     global buffer
@@ -22,9 +28,23 @@ def recreate_buffer():
     buffer = [[" " for y in range(height)] for x in range(width)]
 
 def display():
-    width = get_size()[0]
-    height = get_size()[1]
+    global user_input
 
+    # Clear the screen and render the buffer
+    clear()
+    recreate_buffer()
+    render()
+
+    # Draw the buffer
     for y in range(height):
         for x in range(width):
             print(buffer[x][y], end="", flush=False)
+
+        # Newline
+        # Even though the buffer will overflow to the next line normally,
+        # the console can be resized which will affect how it overflows
+        print("", flush=False)
+    
+    # Flush the buffer and print a cursor so the user can give input
+    print(" > ", end="", flush=True)
+    user_input = input()
