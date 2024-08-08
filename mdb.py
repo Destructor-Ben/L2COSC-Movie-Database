64586ead6f8d96c414fc075478ece0dfdf5210ce
Destructor_Ben <ben.honeyfield@gmail.com>
2024-08-09 10:15:57 +1200

    Test capabilities of the console

--- mdb.py
+++ mdb.py
@@ -2,16 +2,24 @@ import mdb_console as console
 import time
 
 console.clear()
-print(console.get_size())
-
-time.sleep(1)
-
-console.clear()
 console.recreate_buffer()
+
 # Rendering goes here
-console.buffer[0][0] = "a"
+for x in range(console.width):
+    console.buffer[x][0] = "-"
+    console.buffer[x][-1] = "-"
+
+for y in range(console.height):
+    console.buffer[0][y] = "|"
+    console.buffer[-1][y] = "|"
+
+console.buffer[0][0] = "+"
+console.buffer[-1][0] = "+"
+console.buffer[0][-1] = "+"
+console.buffer[-1][-1] = "+"
+
 console.display()
 
-time.sleep(2)
+input()
 
 console.clear()
