80bc1dd16fb22c38612e179bf8d36428320bcd84
Destructor_Ben <ben.honeyfield@gmail.com>
2024-09-25 22:40:07 +1200

    Rewrite the movie page.

--- src/pages/movie.py
+++ src/pages/movie.py
@@ -3,10 +3,10 @@
 import mdb_console as console
 import mdb_database as db
 import mdb_ui as ui
-from mdb_commands import Command, commands
+from mdb_commands import Command, commands, get_available_commands
+from mdb_movie import MovieField, Movie
 
 
-# TODO: improve this
 class MoviePage(ui.Page):
     """The movie page of the UI."""
 
@@ -33,23 +33,87 @@ class MoviePage(ui.Page):
             self.error_message = f"A movie with an ID of '{self.movie_id}' doesn't exist"
             return
 
-        # Draw the movie info
-        x = 2
-        y = 2
+        # Make a list of the fields
+        fields = [["ID", str(self.movie.id)]]
 
-        console.write(x, y, self.movie.id)
+        if self.movie.release_year is not None:
+            fields.append(["Release Year", str(self.movie.release_year)])
+
+        if self.movie.audience_rating is not None:
+            fields.append(["Audience Rating", str(self.movie.audience_rating)])
+
+        if self.movie.runtime is not None:
+            fields.append(["Runtime", str(self.movie.runtime) + "m"])
+
+        if self.movie.genre is not None:
+            fields.append(["Genres", ", ".join(map(lambda g: str(g), self.movie.genre))])
+
+        if self.movie.star_rating is not None:
+            fields.append(["Star Rating", str(self.movie.star_rating_string)])
+
+        if self.movie.where_to_watch is not None:
+            fields.append(["Where to Watch", str(self.movie.where_to_watch)])
+
+        # Calculate the heifhr of the panel - width of the borders, 7 fields, and the name + a gap
+        height = 2 + len(fields) + 2
+
+        # Calculate the width of the panel
+        # This is the longest of the fields and the movie name
+        width = len(self.movie.name)
+
+        for field in fields:
+            field_width = len(f"{field[0]}: {field[1]}")
+            if field_width > width:
+                width = field_width
+
+        # Align the fields
+        for field in fields:
+            field_width = len(f"{field[0]}: {field[1]}")
+            field[1] = (width - field_width) * " " + field[1]
+
+        # Add 2 to the width for the border and another 2 for padding
+        width += 4
+
+        # Calculate the width of the command panel
+        command_x = 0
+        commands_list = get_available_commands()
+        if len(commands_list) > 0:
+            # Calculate the widest command and add padding
+            widest_command = max([len(str(command)) for command in commands_list + ["Commands"]])
+            widest_command += 2
+            command_x = -widest_command
+
+        # Calculate the x and y position
+        x = (console.width - width + command_x) // 2
+        y = (console.height - height) // 2
+
+        # Draw a border
+        for border_x in range(width):
+            console.set(x + border_x, y, ui.HORIZONTAL_BAR_CHAR)
+            console.set(x + border_x, y + height - 1, ui.HORIZONTAL_BAR_CHAR)
+
+        for border_y in range(height):
+            console.set(x, y + border_y, ui.VERTICAL_BAR_CHAR)
+            console.set(x + width - 1, y + border_y, ui.VERTICAL_BAR_CHAR)
+
+        console.set(x, y, ui.CORNER_BAR_CHARS[0])
+        console.set(x + width - 1, y, ui.CORNER_BAR_CHARS[1])
+        console.set(x, y + height - 1, ui.CORNER_BAR_CHARS[2])
+        console.set(x + width - 1, y + height - 1, ui.CORNER_BAR_CHARS[3])
+
+        # Shift the fields down and right
+        x += 2
         y += 1
-        console.write(x, y, self.movie.name)
-        y += 1
-        console.write(x, y, self.movie.release_year)
-        y += 1
-        console.write(x, y, self.movie.audience_rating)
-        y += 1
-        console.write(x, y, self.movie.runtime)
-        y += 1
-        console.write(x, y, ", ".join(map(lambda g: str(g), self.movie.genre)))
-        y += 1
-        console.write(x, y, self.movie.star_rating)
-        y += 1
-        console.write(x, y, self.movie.where_to_watch)
-        y += 1
+
+        # Name and blank gap
+        name = self.movie.name
+        name_x_offset = (width - len(name) - 2) // 2
+        console.write(x + name_x_offset, y, name, ui.COLOUR_GREEN)
+        y += 2
+
+        # Rest of the fields
+        for field in fields:
+            prefix = f"{field[0]}: "
+            console.write(x, y, prefix, ui.COLOUR_BLUE)
+            console.write(x + len(prefix), y, field[1])
+            y += 1
