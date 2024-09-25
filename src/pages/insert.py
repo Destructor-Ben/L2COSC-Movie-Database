257afd2833d78563996318506537a02e2640cfaf
Destructor_Ben <ben.honeyfield@gmail.com>
2024-09-25 22:40:48 +1200

    Fix the insert page and add input validation.

--- src/pages/insert.py
+++ src/pages/insert.py
@@ -1,25 +1,23 @@
 """The insert page of the UI."""
 
-# TODO: implement - This won't have any args in the command
-
 import mdb_console as console
 import mdb_database as db
 import mdb_ui as ui
 from mdb_commands import Command, commands
-from mdb_movie import Movie
+from mdb_movie import AudienceRating, Genre, MovieField, Movie
 
 
 class InsertPage(ui.Page):
     """The insert page of the UI."""
 
-    ATTRIBUTES = [
-        "name",
-        "release year",
-        "audience rating",
-        "runtime",
-        "genre",
-        "star rating",
-        "where to watch",
+    MOVIE_FIELDS = [
+        MovieField.NAME,
+        MovieField.RELEASE_YEAR,
+        MovieField.AUDIENCE_RATING,
+        MovieField.RUNTIME,
+        MovieField.GENRE,
+        MovieField.STAR_RATING,
+        MovieField.WHERE_TO_WATCH,
     ]
 
     @staticmethod
@@ -31,33 +29,81 @@ class InsertPage(ui.Page):
         """Create a page."""
         super().__init__("Insert", 10, 10)
         self.getting_input = True
-        self.current_attribute = -1
-        self.movie_attributes = []
+        # The index in MOVIE_FIELDS we are currently searching for
+        self.current_field_index = 0
+        # Dictionary of all of the movie fields
+        self.movie_fields = {}
+        # Used to render the final page
+        self.movie_added = False
+        self.movie = None
+        # Stops us wrongly using "insert" as the initial user input
+        self.first_open = True
 
     @staticmethod
     def command_insert():
         """Go to the insert page."""
         ui.current_page = InsertPage()
 
+    @property
+    def current_field(self) -> MovieField:
+        """Get the enum for current field the user is inputting."""
+        return InsertPage.MOVIE_FIELDS[self.current_field_index]
+
+    def on_finish_input(self):
+        """Add the movie to the database once the user is done giving input."""
+        # Add the movie
+        movie = Movie(0, *self.movie_fields.values())
+        movie_id = db.insert(movie)
+
+        # Get the movie (with the updated ID from inserting it)
+        self.getting_input = False
+        self.movie_added = True
+        self.movie = db.get(movie_id)
+
+    def get_prompt(self):
+        """Get the prompt for the user for the given movie field."""
+        return self.current_field.get_insert_prompt()
+
+    # TODO: pressing enter after the movie is added will crash the program
+    # TODO: when there is an error, the messages aren't reprinted
     def render(self):
         """Render the page."""
-        # If we just asked the user for input, add it
-        if self.current_attribute > -1:
-            # TODO: validate the input
-            self.movie_attributes.append(console.user_input)
+        message_x = 2
+        message_y = 2
 
-        # Get the next attribute for the movie
-        self.current_attribute += 1
+        # Draw the info showing the after it is added
+        if self.movie_added:
+            console.write(message_x, message_y, "Movie successfully added:", ui.COLOUR_BLUE)
+            console.write(message_x, message_y + 1, self.movie)
+            return
 
-        # Add the movie if we have reached the end, otherwise ask for the next one
-        if self.current_attribute >= len(InsertPage.ATTRIBUTES):
-            # TODO: add the movie then get it to be displayed, since the ID will change
-            movie = Movie(0, *self.movie_attributes)
-            db.insert(movie)
-            message = f"Movie successfully added: {movie}"
-            self.getting_input = False
-        else:
-            message = f"What is the {InsertPage.ATTRIBUTES[self.current_attribute]} of the movie?"
+        # Draw the prompt for the user
+        message = self.get_prompt()
+        for line in message.split("\n"):
+            console.write(message_x, message_y, line, ui.COLOUR_BLUE)
+            message_y += 1
 
-        # Write message
-        console.write(2, 2, message, ui.COLOUR_BLUE)
+        # Don't use input if the user hasn't even been asked yet
+        if self.first_open:
+            self.first_open = False
+            return
+
+        # If we just asked the user for input, get it and validate it
+        # Validate the input
+        (is_valid, user_input, error_message) = self.current_field.validate_field(console.user_input.strip())
+
+        # Return if it isn't valid
+        if is_valid == False:
+            self.error_message = error_message
+            return
+
+        # Add the field
+        self.movie_fields[self.current_field] = user_input
+
+        # Get the next field for the movie
+        # TODO: the movie index is wrong and offset by 1
+        self.current_field_index += 1
+        
+        # Add the movie if we have reached the end of the required fields
+        if self.current_field_index >= len(InsertPage.MOVIE_FIELDS):
+            self.on_finish_input()
