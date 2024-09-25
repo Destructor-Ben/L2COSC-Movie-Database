3521596bed550ac11d7ca63bc442b3c7f789f93d
Destructor_Ben <ben.honeyfield@gmail.com>
2024-09-25 22:55:04 +1200

    Update mdb_ui.py

--- src/mdb_ui.py
+++ src/mdb_ui.py
@@ -1,8 +1,5 @@
 """The code related to managing the individual pages of the UI."""
 
-# TODO: implement minimum sizes
-# TODO: go over all error messages
-
 import mdb_commands as commands
 import mdb_console as console
 
