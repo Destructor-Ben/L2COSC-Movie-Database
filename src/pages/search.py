ab182ffe81490a74c9cb3ec221bbad5c5c1e2480
Destructor_Ben <ben.honeyfield@gmail.com>
2024-09-25 22:40:30 +1200

    Add proper search validation.

--- src/pages/search.py
+++ src/pages/search.py
@@ -4,44 +4,44 @@ import mdb_database as db
 import mdb_ui as ui
 from pages.movies import AllMoviesPage
 from mdb_commands import Command, commands
+from mdb_movie import MovieField
 
 
 # The code for this is basically the same as the movie list, so we copy it
 class SearchPage(AllMoviesPage):
     """The search page of the UI."""
 
-    ATTRIBUTES = [
-        "name",
-        "release_year",
-        "audience_rating",
-        "runtime",
-        "genre",
-        "star_rating",
-        "where_to_watch",
-    ]
-
     @staticmethod
     def setup():
         """Initialize the page."""
         commands.append(Command("search", SearchPage.command_search))
 
-    def __init__(self, attribute, query):
+    def __init__(self, field, query):
         """Create a page."""
         super().__init__()
         self.name = "Search"
-        self.attribute = attribute
+        self.field = field
         self.query = query
 
     @staticmethod
-    def command_search(attribute, query):
+    def command_search(field, query):
         """Go to the search page."""
-        #if attribute.lower().strip() not in SearchPage.ATTRIBUTES:
-            #ui.current_page.error_message = f"Invalid attribute ({attribute})"
-            #return
+        # Check that the given movie field exists
+        movie_field = MovieField.from_str(field)
+        if movie_field is None:
+            # TODO: give user feedback for what is a valid field
+            ui.current_page.error_message = f"Invalid movie field ({field})"
+            return
 
-        # TODO: validate query
+        # Check that the query itself is valid
+        (is_valid, _, error_message) = movie_field.validate_field(query)
+        if not is_valid:
+            ui.current_page.error_message = f"Invalid movie query ({query}) - {error_message}"
+            return
 
-        ui.current_page = SearchPage(attribute, query)
+        # Go to the page
+        ui.current_page = SearchPage(movie_field, query)
 
     def get_movies(self):
-        return db.get_filter(self.attribute, self.query)
+        """Get the movies to be displayed."""
+        return db.get_filter(self.field.database_name, self.query)
